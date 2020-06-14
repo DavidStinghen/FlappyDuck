4146646ab4ae64d7bcaa267f113877eb2ca6a82e
DavidStinghen <davidstinghen@gmail.com>
2020-06-14 19:11:38 -0300

    chore: add images of duck, floor and pipes

---
+++ src/assets/Images.js
@@ -0,0 +1,9 @@
+export default Images = {
+  background: require('./img/background.png'),
+  floor: require('./img/floor.png'),
+  pipeBottom: require('./img/pipe_bottom.png'),
+  pipeTop: require('./img/pipe_top.png'),
+  duck1: require('./img/duck1.png'),
+  duck2: require('./img/duck2.png'),
+  duck3: require('./img/duck3.png'),
+};
