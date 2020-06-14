ce5ceaf8d7696c68b6fb0dec3f4002747bd1ad58
DavidStinghen <davidstinghen@gmail.com>
2020-06-14 19:10:02 -0300

    chore: create constants for metrics

---
+++ src/helpers/Constants.js
@@ -0,0 +1,12 @@
+import {Dimensions} from 'react-native';
+
+const Constants = {
+  MAX_WIDTH: Dimensions.get('screen').width,
+  MAX_HEIGHT: Dimensions.get('screen').height,
+  GAP_SIZE: 320,
+  PIPE_WIDTH: 100,
+  DUCK_WIDTH: 50,
+  DUCK_HEIGHT: 41,
+};
+
+export default Constants;
